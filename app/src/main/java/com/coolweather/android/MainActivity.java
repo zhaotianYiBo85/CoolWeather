408de47f4bb30d64b1d8b5ce56aa4d6e8c68d8a4
zhaotianYiBo85 <1846548660@qq.com>
2020-04-22 14:41:40 +0800

    weather info three

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.coolweather.android;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // 从 SharedPreferences 中读取缓存数据
+        if (prefs.getString("weather", null) != null) {
+            // 之前请求过则直接跳转到天气信息
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
